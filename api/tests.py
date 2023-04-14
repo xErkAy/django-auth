1cc733896efe94bb2f0b43036cc011534a9bba39
Erik <e.arstamyan@bk.ru>
2023-04-14 13:31:39 +0300

    updated urls, added tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+from project.models import User
 
-# Create your tests here.
+class CustomTest(TestCase):
+    def test_user_creation(self):
+        User.objects.create_user(username='test_user', password='test_password')
+        user = User.objects.get(username='test_user')
+        self.assertTrue(user.check_password('test_password'))
+
+    def test_auth(self):
+        User.objects.create_user(username='test_user', password='test_password')
+        data = {
+            'username': 'test_user',
+            'password': 'test_password'
+        }
+        response = self.client.post(reverse('user_auth'), data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_using_header(self):
+        User.objects.create_user(username='test_user', password='test_password')
+        data = {
+            'username': 'test_user',
+            'password': 'test_password'
+        }
+        response = self.client.post(reverse('user_auth'), data)
+        token = response.data.get('token')
+
+        headers = {
+            'Authorization': f'Bearer {token}',
+        }
+
+        response = self.client.post(reverse('test_view'), data={}, headers=headers)
+        self.assertEqual(response.data.get('text'), 123)
--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from api.views import AuthenticationAPIView, TestAPIView, RegistrationAPIView
 
 urlpatterns = [
-    path('auth/', AuthenticationAPIView.as_view()),
-    path('registration/', RegistrationAPIView.as_view()),
-    path('test/', TestAPIView.as_view())
+    path('auth/', AuthenticationAPIView.as_view(), name='user_auth'),
+    path('registration/', RegistrationAPIView.as_view(), name='user_register'),
+    path('test/', TestAPIView.as_view(), name='test_view')
 ]
